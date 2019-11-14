2b9c53cd0cfa37e3e40ab2286bc34f08b9beb46c
Kerk Dovan <bogomolov.yuriy@gmail.com>
2019-11-14 19:51:46 +0300

    In image generator: print by one char at time (with crutches: seems to be a bug [or feature] in Pillow library :c); own font settings for each line; fixed width (4px) for separator layer marks; layers are hidden by default.

--- image_generator/generate.py
+++ image_generator/generate.py
@@ -1,4 +1,5 @@
 import random
+import string
 from collections import namedtuple
 
 from PIL import Image, ImageDraw, ImageFont
@@ -21,40 +22,62 @@ def generate(width, height):
         for i, x in enumerate(image._fields)}
     )
 
-    text = 'こんにちは,\nHoàng Tùng Lâm!\nМеня зовут Kerk Dovan.'
-    font_size = 42
-    font = ImageFont.truetype('yumin.ttf', size=font_size)
-    t_width, t_height = font.getsize_multiline(text)
+    fontname = 'verdana.ttf'
+    basefontsize = 40
+    punct1 = u' '.join(string.punctuation[:len(string.punctuation) // 2])
+    punct2 = u' '.join(string.punctuation[len(string.punctuation) // 2:])
+    text = [
+        (u'А Б В Г Д Е Ё Ж З И Й К Л М Н О П', ImageFont.truetype(fontname, basefontsize)),
+        (u'Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я', ImageFont.truetype(fontname, basefontsize)),
+        (u'а б в г д е ё ж з и й к л м н о п', ImageFont.truetype(fontname, basefontsize + 4)),
+        (u'р с т у ф х ц ч ш щ ъ ы ь э ю я', ImageFont.truetype(fontname, basefontsize + 4)),
+        (u'A B C D E F G H I J K L M', ImageFont.truetype(fontname, basefontsize + 8)),
+        (u'N O P Q R S T U V W X Y Z', ImageFont.truetype(fontname, basefontsize + 8)),
+        (u'a b c d e f g h i j k l m', ImageFont.truetype(fontname, basefontsize + 14)),
+        (u'n o p q r s t u v w x y z', ImageFont.truetype(fontname, basefontsize + 14)),
+        (punct1, ImageFont.truetype(fontname, basefontsize)),
+        (punct2, ImageFont.truetype(fontname, basefontsize)),
+        (u'0 1 2 3 4 5 6 7 8 9', ImageFont.truetype(fontname, basefontsize)),
+    ]
+    t_width, t_height = 0, 0
+    for line, font in text:
+        tw, th = font.getsize(line)
+        t_width = max(t_width, tw)
+        t_height += th
 
     x = random.randint(0, width - t_width)
     y = random.randint(0, height - t_height)
-    draw.image.text((x, y), text, fill=(0, 0, 0), font=font)
 
     dy = 0
-    for line in text.split('\n'):
+    for line, font in text:
         lw, lh = font.getsize(line)
         lh2, lh4 = lh // 2, lh // 4
         lh34 = lh2 + lh4
 
+        dx = 0
         for i, char in enumerate(line):
             c_width, c_height = font.getsize(line[i])
             w, h = font.getmask(char).size
+            if w != c_width and i == 0:
+                dx = w - c_width
 
             right, bottom = font.getsize(line[:i + 1])
             right += x
             bottom = min(c_height, bottom) + y + dy
             top = bottom - h
-            left = right - w
+            left = right - c_width
 
             if not char.isspace():
+                draw.image.text(
+                    (left - dx + (w - c_width), y + dy), char, fill=(0, 0, 0), font=font)
                 draw.char_box.rectangle(
-                    (left, top, right, bottom), fill=(0, 0, 200, 50))
+                    (left - dx, top, right, bottom), fill=(0, 0, 200, 50))
 
             if i == len(line) - 1:
                 continue
 
             draw.separator.rectangle(
-                (right - w // 4, y + dy, right, y + dy + lh), fill=(200, 0, 200, 50))
+                (right - 2, y + dy, right + 2, y + dy + lh), fill=(200, 0, 200, 50))
 
         draw.line_top.rectangle(
             (x, y + dy, x + lw, y + dy + lh2), fill=(200, 0, 0, 50))
@@ -66,5 +89,5 @@ def generate(width, height):
         dy += font.getsize_multiline(f'\nA')[1] - font.getsize('A')[1]
 
     return {
-        **{x: image[i] for i, x in enumerate(image._fields)}
+        x: image[i] for i, x in enumerate(image._fields)
     }
