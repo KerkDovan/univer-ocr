ee8688828854a99490d20378e9db150298004ea8
Kerk Dovan <bogomolov.yuriy@gmail.com>
2019-11-17 01:04:20 +0300

    Refactor image generation and make data markup precise as much as possible; list fonts.

--- image_generator/generate.py
+++ image_generator/generate.py
@@ -1,93 +1,252 @@
 import random
 import string
-from collections import namedtuple
 
+import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
 
-def generate(width, height):
-    image_tuple = namedtuple(
-        'image_tuple', 'image line_top line_center line_bottom separator char_box')
+RUSSIAN_LOWERCASE = u'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+RUSSIAN_UPPERCASE = u'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+RUSSIAN = RUSSIAN_LOWERCASE + RUSSIAN_UPPERCASE
+CHARS = RUSSIAN + string.digits + string.ascii_letters + string.punctuation + ' '
 
-    image = image_tuple(
-        image=Image.new('RGBA', (width, height), (200, 200, 200, 255)),
-        line_top=Image.new('RGBA', (width, height)),
-        line_center=Image.new('RGBA', (width, height)),
-        line_bottom=Image.new('RGBA', (width, height)),
-        separator=Image.new('RGBA', (width, height)),
-        char_box=Image.new('RGBA', (width, height)),
-    )
-    draw = image_tuple(**{
-        x: ImageDraw.ImageDraw(image[i])
-        for i, x in enumerate(image._fields)}
-    )
 
-    fontname = 'verdana.ttf'
-    basefontsize = 40
-    punct1 = u' '.join(string.punctuation[:len(string.punctuation) // 2])
-    punct2 = u' '.join(string.punctuation[len(string.punctuation) // 2:])
-    text = [
-        (u'А Б В Г Д Е Ё Ж З И Й К Л М Н О П', ImageFont.truetype(fontname, basefontsize)),
-        (u'Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я', ImageFont.truetype(fontname, basefontsize)),
-        (u'а б в г д е ё ж з и й к л м н о п', ImageFont.truetype(fontname, basefontsize + 4)),
-        (u'р с т у ф х ц ч ш щ ъ ы ь э ю я', ImageFont.truetype(fontname, basefontsize + 4)),
-        (u'A B C D E F G H I J K L M', ImageFont.truetype(fontname, basefontsize + 8)),
-        (u'N O P Q R S T U V W X Y Z', ImageFont.truetype(fontname, basefontsize + 8)),
-        (u'a b c d e f g h i j k l m', ImageFont.truetype(fontname, basefontsize + 14)),
-        (u'n o p q r s t u v w x y z', ImageFont.truetype(fontname, basefontsize + 14)),
-        (punct1, ImageFont.truetype(fontname, basefontsize)),
-        (punct2, ImageFont.truetype(fontname, basefontsize)),
-        (u'0 1 2 3 4 5 6 7 8 9', ImageFont.truetype(fontname, basefontsize)),
-    ]
-    t_width, t_height = 0, 0
-    for line, font in text:
-        tw, th = font.getsize(line)
-        t_width = max(t_width, tw)
-        t_height += th
-
-    x = random.randint(0, width - t_width)
-    y = random.randint(0, height - t_height)
-
-    dy = 0
-    for line, font in text:
-        lw, lh = font.getsize(line)
-        lh2, lh4 = lh // 2, lh // 4
-        lh34 = lh2 + lh4
-
-        dx = 0
-        for i, char in enumerate(line):
-            c_width, c_height = font.getsize(line[i])
-            w, h = font.getmask(char).size
-            if w != c_width and i == 0:
-                dx = w - c_width
-
-            right, bottom = font.getsize(line[:i + 1])
-            right += x
-            bottom = min(c_height, bottom) + y + dy
-            top = bottom - h
-            left = right - c_width
-
-            if not char.isspace():
-                draw.image.text(
-                    (left - dx + (w - c_width), y + dy), char, fill=(0, 0, 0), font=font)
-                draw.char_box.rectangle(
-                    (left - dx, top, right, bottom), fill=(0, 0, 200, 50))
-
-            if i == len(line) - 1:
-                continue
-
-            draw.separator.rectangle(
-                (right - 2, y + dy, right + 2, y + dy + lh), fill=(200, 0, 200, 50))
-
-        draw.line_top.rectangle(
-            (x, y + dy, x + lw, y + dy + lh2), fill=(200, 0, 0, 50))
-        draw.line_center.rectangle(
-            (x, y + dy + lh4, x + lw, y + dy + lh34), fill=(200, 200, 0, 50))
-        draw.line_bottom.rectangle(
-            (x, y + dy + lh2, x + lw, y + dy + lh), fill=(0, 200, 0, 50))
-
-        dy += font.getsize_multiline(f'\nA')[1] - font.getsize('A')[1]
-
+def safe_char(char):
+    assert len(char) == 1
+    if char not in CHARS:
+        return 'unknown'
+    if char not in string.punctuation + ' ':
+        return char
     return {
-        x: image[i] for i, x in enumerate(image._fields)
+        '!': 'exclamation', '"': 'double_quote', '#': 'hash_sign', '$': 'dollar_sign',
+        '%': 'percent', '&': 'ampersand', '\'': 'single_quote', '(': 'op_parentheses',
+        ')': 'cl_parentheses', '*': 'asterisk', '+': 'plus', ',': 'comma', '-': 'minus',
+        '.': 'point', '/': 'slash', ':': 'colon', ';': 'semicolon', '<': 'less', '=': 'equal',
+        '>': 'more', '?': 'question', '@': 'at_sign', '[': 'op_square', '\\': 'backslash',
+        ']': 'cl_square', '^': 'caret', '_': 'underscore', '`': 'apostrophe', '{': 'op_curly',
+        '|': 'vertical_bar', '}': 'cl_curly', '~': 'tilde', ' ': 'space',
+    }.get(char, 'unknown')
+
+
+class LayeredImage:
+    layer_names = [
+        'image',
+        'paragraph',
+        'line_top',
+        'line_center',
+        'line_bottom',
+        'letter_spacing',
+        'char_box',
+        'char_box1',
+        *[f'char_box_{safe_char(c)}' for c in CHARS],
+    ]
+    colors = {
+        'image': (0, 0, 0, 255),
+        **{layer: 255 for layer in layer_names},
     }
+    colors_demo = {
+        'image': (0, 0, 0, 255),
+        'paragraph': (0, 0, 200, 50),
+        'line_top': (200, 0, 0, 100),
+        'line_center': (0, 0, 200, 150),
+        'line_bottom': (0, 200, 0, 100),
+        'letter_spacing': (200, 0, 200, 100),
+        'char_box': (200, 200, 0, 100),
+        'char_box1': (200, 200, 0, 100),
+    }
+
+    def __init__(self, width, height, bg_color, use_demo):
+        self.width, self.height = width, height
+        self.bg_color = bg_color
+        self.use_demo = use_demo
+        self.layers = {
+            'image': Image.new('RGBA', (self.width, self.height), self.bg_color),
+            **{
+                name: Image.new('L', (self.width, self.height))
+                for name in self.layer_names[1:]
+            }
+        }
+        self.mask = None
+        self._updage_mask()
+        self.draw = {
+            name: ImageDraw.ImageDraw(layer)
+            for name, layer in self.layers.items()
+        }
+        self.demo = {
+            'image': Image.new('RGBA', (self.width, self.height), self.bg_color),
+            **{
+                name: Image.new('RGBA', (self.width, self.height))
+                for name in self.layer_names[1:]
+            }
+        } if self.use_demo else {}
+        self.draw_demo = {
+            name: ImageDraw.ImageDraw(layer)
+            for name, layer in self.demo.items()
+        }
+
+    def get_images(self):
+        return self.layers
+
+    def get_demo(self):
+        return self.demo
+
+    def add_paragraph(self, text):
+        t_width, t_height = 0, 0
+        for line, font in text:
+            spacing = font.size // 2
+            offset_x, _ = font.getoffset(line + CHARS)
+            t_width = max(t_width, font.font.getsize(line)[0][0] + offset_x)
+            t_height += font.getsize_multiline(
+                f'{line}\nA', spacing=spacing
+            )[1] - font.getsize('A')[1]
+
+        ones = np.ones((t_height, t_width))
+        x, y = None, None
+        while x is None or np.sum(self.mask[y:y + t_height, x:x + t_width] * ones) > 0:
+            x = random.randint(20, self.width - t_width - 20)
+            y = random.randint(0, self.height - t_height)
+
+        self._paragraph((x, y, x + t_width, y + t_height))
+        self._updage_mask()
+
+        dy = 0
+        for line, font in text:
+            spacing = font.size // 2
+            ascent, descent = font.getmetrics()
+            M_height, x_height = font.getmask('M').size[1], font.getmask('x').size[1]
+            height = font.font.getsize(CHARS)[0][1]
+            width = font.font.getsize(line)[0][0]
+            offset_x, offset_y = font.getoffset(line + CHARS)
+
+            left = x + offset_x
+            right = left + width + offset_x
+
+            y_ascent = y + dy + offset_y
+            y_baseline = y_ascent + height - descent
+            y_M = y_baseline - M_height
+            y_x = y_baseline - x_height
+            y_descent = y_baseline + descent
+
+            _, lh = font.getsize(line)
+            self._line(left, right, y_ascent, y_M, y_x, y_baseline, y_descent)
+
+            for i, char in enumerate(line):
+                c_width, c_height = font.getsize(line[i])
+                w, h = font.getmask(char).size
+
+                ch_r, ch_b = font.getsize(line[:i + 1])
+                ch_r += offset_x
+                ch_b = min(c_height, ch_b) + y + dy
+                ch_t = ch_b - h
+                ch_l = ch_r - c_width
+                ch_offset_x = font.getoffset(char)[0]
+
+                self._char(char, (x + ch_l - ch_offset_x, y + dy), font)
+                self._box(char, (x + ch_l, ch_t, x + ch_r, ch_b))
+                self._box1(char, (x + ch_l, y_ascent, x + ch_r, y_descent))
+
+                if i == len(line) - 1:
+                    continue
+
+                self._letter_spacing((x + ch_r - 2, y_ascent, x + ch_r + 2, y_descent))
+
+            dy += font.getsize_multiline(f'{line}\nA', spacing=spacing)[1] - font.getsize('A')[1]
+
+    def _paragraph(self, coords):
+        self.draw['paragraph'].rectangle(coords, fill=self.colors['paragraph'])
+        if self.use_demo:
+            self.draw_demo['paragraph'].rectangle(
+                coords, fill=self.colors_demo['paragraph'])
+
+    def _char(self, char, position, font):
+        self.draw['image'].text(position, char, fill=self.colors['image'], font=font)
+        if self.use_demo:
+            self.draw_demo['image'].text(
+                position, char, fill=self.colors_demo['image'], font=font)
+
+    def _box(self, char, coords):
+        char = safe_char(char)
+        self.draw['char_box'].rectangle(coords, fill=self.colors['char_box'])
+        self.draw['char_box_' + char].rectangle(
+            coords, fill=self.colors['char_box'])
+        if self.use_demo:
+            self.draw_demo['char_box'].rectangle(
+                coords, fill=self.colors_demo['char_box'])
+            self.draw_demo['char_box_' + char].rectangle(
+                coords, fill=self.colors_demo['char_box'])
+
+    def _box1(self, char, coords):
+        char = safe_char(char)
+        self.draw['char_box1'].rectangle(coords, fill=self.colors['char_box1'])
+        if self.use_demo:
+            self.draw_demo['char_box1'].rectangle(
+                coords, fill=self.colors_demo['char_box1'])
+
+    def _letter_spacing(self, coords):
+        self.draw['letter_spacing'].rectangle(coords, fill=self.colors['letter_spacing'])
+        if self.use_demo:
+            self.draw_demo['letter_spacing'].rectangle(
+                coords, fill=self.colors_demo['letter_spacing'])
+
+    def _line(self, left, right, y_ascent, y_M, y_x, y_baseline, y_descent):
+        line_top_coords = (left, y_ascent, right, y_baseline)
+        line_center_coords = (left, y_x, right, y_baseline)
+        line_bottom_coords = (left, y_x, right, y_descent)
+
+        self.draw['line_top'].rectangle(line_top_coords, fill=self.colors['line_top'])
+        self.draw['line_center'].rectangle(line_center_coords, fill=self.colors['line_center'])
+        self.draw['line_bottom'].rectangle(line_bottom_coords, fill=self.colors['line_bottom'])
+
+        if self.use_demo:
+            def hline(y, color):
+                self.draw_demo['image'].line((left, y, right, y), fill=color, width=1)
+
+            hline(y_ascent, (200, 0, 200))
+            hline(y_M, (0, 200, 0))
+            hline(y_x, (0, 200, 200))
+            hline(y_baseline, (200, 0, 0))
+            hline(y_descent, (0, 0, 200))
+
+            self.draw_demo['line_top'].rectangle(
+                line_top_coords, fill=self.colors_demo['line_top'])
+            self.draw_demo['line_center'].rectangle(
+                line_center_coords, fill=self.colors_demo['line_center'])
+            self.draw_demo['line_bottom'].rectangle(
+                line_bottom_coords, fill=self.colors_demo['line_bottom'])
+
+    def _updage_mask(self):
+        self.mask = np.array(self.layers['paragraph'])
+
+
+def generate(width, height, use_demo=False):
+    layers = LayeredImage(width, height, (200, 200, 200, 255), use_demo)
+
+    fontname = 'times.ttf'
+    basefontsize = 32
+    text1 = [
+        (line, ImageFont.truetype(fontname, basefontsize + add))
+        for line, add in [
+            (RUSSIAN_LOWERCASE[:-5], 0),
+            (RUSSIAN_LOWERCASE[-5:] + RUSSIAN_UPPERCASE[:-13], 0),
+            (RUSSIAN[-13:] + string.digits, 0),
+            (string.ascii_lowercase, 0),
+            (string.ascii_uppercase, 0),
+            (string.punctuation + ' ', 0),
+            ('jklmnopqЙЁ_-^', 0),
+        ]
+    ]
+    text2 = [
+        (line, ImageFont.truetype(fontname, basefontsize + add))
+        for line, add in [
+            (u'Съешь же ещё этих мягких', 0),
+            (u'французских булок, да выпей чаю.', 0),
+            (u'The quick brown fox ' + string.digits, 0),
+            (u'jumps over the lazy dog.', 0),
+            (string.punctuation, 0),
+        ]
+    ]
+    texts = [text1, text2]
+    for _ in range(5):
+        layers.add_paragraph(random.choice(texts))
+
+    return layers.get_images(), layers.get_demo()
--- web_app/views/main.py
+++ web_app/views/main.py
@@ -1,16 +1,32 @@
+from collections import namedtuple
+
 from flask import Blueprint, render_template, send_file
 
 from web_app import image_generator as ig
 
 main_bp = Blueprint('main_bp', __name__)
+
 generated = None
+
+Font = namedtuple('Font', 'name normal bold italic bold_italic')
+used_fonts = [
+    Font('Arial', 'arial.ttf', 'arialbd.ttf', 'ariali.ttf', 'arialbi.ttf'),
+    Font('Arial Narrow', 'ARIALN.TTF', 'ARIALNB.TTF', 'ARIALNI.TTF', 'ARIALNBI.TTF'),
+    Font('Calibri', 'calibri.ttf', 'calibrib.ttf', 'calibrii.ttf', 'calibriz.ttf'),
+    Font('Calibri Light', 'calibril.ttf', None, 'calibrili.ttf', None),
+    Font('Cambria', 'cambria.ttc', 'cambriab.ttf', 'cambriai.ttf', 'cambriaz.ttf'),
+    Font('Comic Sans', 'comic.ttf', 'comicbd.ttf', 'comici.ttf', 'comicz.ttf'),
+    Font('Consolas', 'consola.ttf', 'consolab.ttf', 'consolai.ttf', 'consolaz.ttf'),
+    Font('Times New Roman', 'times.ttf', 'timesbd.ttf', 'timesi.ttf', 'timesbi.ttf'),
+    Font('Verdana', 'verdana.ttf', 'verdanab.ttf', 'verdanai.ttf', 'verdanaz.ttf'),
+]
 
 
 @main_bp.route('/')
 @main_bp.route('/index')
 def index():
     global generated
-    generated = ig.generate(800, 600)
+    generated = ig.generate(1920, 1080, True)[1]
     context = {
         'layer_names': list(generated.keys()),
     }
@@ -23,3 +39,11 @@ def image(image_type):
         ig.to_bytesio(generated[image_type]),
         mimetype='image/png',
         cache_timeout=0)
+
+
+@main_bp.route('/fonts')
+def fonts():
+    context = {
+        'fonts': used_fonts,
+    }
+    return render_template('fonts.html', **context)
