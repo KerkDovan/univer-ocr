4c66e6c4e079b65ea97230721b7c21942a3c6d71
Kerk Dovan <bogomolov.yuriy@gmail.com>
2019-11-18 03:31:07 +0300

    Add view for used characters; add spacing between navbar menu items.

--- web_app/views/main.py
+++ web_app/views/main.py
@@ -1,7 +1,7 @@
 from flask import render_template, send_file
 
 from ..components import image_generator as ig
-from ..components.primitives import FONTS_LIST
+from ..components.primitives import CHARS, FONTS_LIST, encode_char
 from . import main_bp
 
 generated = None
@@ -24,6 +24,14 @@ def image(image_type):
         ig.to_bytesio(generated[image_type]),
         mimetype='image/png',
         cache_timeout=0)
+
+
+@main_bp.route('/chars')
+def chars():
+    context = {
+        'chars': {x: encode_char(x) for x in CHARS},
+    }
+    return render_template('chars.html', **context)
 
 
 @main_bp.route('/fonts')
