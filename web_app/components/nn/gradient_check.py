Fix error in backward of Upsampling 2D layer; rename prediction argument in loss calls; make more detailed output on gradient check error.

web_app/components/nn/gradient_check.py
@@ -34,9 +34,10 @@ def check_gradient(f, x, delta=1e-5, tol=1e-4):
         numeric_grad_at_ix = (a - b) / (2 * delta)
 
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol).all():
-            print(f"Gradients are different at {ix}. "
-                  f"Analytic: {analytic_grad_at_ix}, "
-                  f"Numeric: {numeric_grad_at_ix}")
+            print(f"Gradients are different at {ix}.\n"
+                  f"  Analytic: {analytic_grad_at_ix},\n"
+                  f"  Numeric: {numeric_grad_at_ix},\n"
+                  f"  Analytic/Numeric: {analytic_grad_at_ix / numeric_grad_at_ix}")
             return False
 
         it.iternext()

web_app/components/nn/layers.py
@@ -401,7 +401,7 @@ class Upsample2D(BaseLayer):
         for y, gy in enumerate(range(0, height, sf_y)):
             for x, gx in enumerate(range(0, width, sf_x)):
                 region = grad[:, gy:gy + sf_y, gx:gx + sf_x, :]
-                result[:, y, x, :] += np.sum(region)
+                result[:, y, x, :] += np.sum(region, axis=(1, 2))
 
         self.clear_memory()
         return result

web_app/components/nn/losses.py
@@ -2,18 +2,18 @@ import numpy as np
 
 
 class BaseLoss:
-    def __call__(self):
+    def __call__(self, prediction, ground_truth):
         raise NotImplementedError()
 
 
 class SigmoidCrossEntropy(BaseLoss):
     """https://gombru.github.io/2018/05/23/cross_entropy_loss/"""
 
-    def __call__(self, X, ground_truth):
+    def __call__(self, prediction, ground_truth):
         def sigmoid(X):
             return 1 / (1 + np.exp(-X))
         batch_size = ground_truth.shape[0]
-        pred = sigmoid(X)
+        pred = sigmoid(prediction)
         gt, rev_gt = ground_truth, 1 - ground_truth
         loss = -(np.sum((gt * np.log(pred)) + rev_gt * np.log(1 - pred))
                  ) / batch_size
@@ -24,13 +24,13 @@ class SigmoidCrossEntropy(BaseLoss):
 class SoftmaxCrossEntropy(BaseLoss):
     """https://deepnotes.io/softmax-crossentropy"""
 
-    def __call__(self, X, ground_truth):
+    def __call__(self, prediction, ground_truth):
         def softmax(X):
             eX = np.exp(X - np.max(X))
             seX = np.transpose([np.sum(eX, axis=1)])
             return eX / seX
         batch_size = ground_truth.shape[0]
-        predicted = softmax(X)
+        predicted = softmax(prediction)
         loss = -(np.sum(ground_truth * np.log(predicted))) / batch_size
         grad = (predicted - ground_truth) / batch_size
         return loss, grad

web_app/components/nn/test/test_gradients.py
@@ -143,11 +143,13 @@ def main():
 
     print('Upsamping 2D Layer')
     X_upsample2d = np.reshape(np.array([
-        [1, 2],
-        [3, 4],
-    ], dtype=np.float), (1, 2, 2, 1))
-    print(X_upsample2d[0, :, :, 0])
-    print(Upsample2D((2, 3)).forward(X_upsample2d)[0, :, :, 0])
+        [0.1, 0.2],
+        [0.3, 0.4],
+    ], dtype=np.float), (1, 2, 2, 1)).repeat(4, axis=0).repeat(3, axis=-1)
+    upsample2d = Upsample2D((2, 3))
+    result = upsample2d.forward(X_upsample2d)
+    grad = upsample2d.backward(result)
+    print(X_upsample2d[0, :, :, 0], result[0, :, :, 0], grad[0, :, :, 0], sep='\n')
     check_layer(Upsample2D(5), X_upsample2d)
 
     X_conv2d = np.random.randn(batch_size, 7, 7, 3)