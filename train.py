5fd2a4e13f78bc5dc51ec31817d0abdc13d9f8ac
Kerk Dovan <bogomolov.yuriy@gmail.com>
2019-12-04 15:39:38 +0300

    Add the ability to run training in console mode.

--- train.py
+++ train.py
@@ -7,18 +7,27 @@ from web_app.components.my_model.train import init_emitter, train_model
 
 
 def main(use_gpu=False):
-    socketIO = SocketIO('127.0.0.1', 80, Namespace=BaseNamespace)
-    client = socketIO.define(BaseNamespace, '/train-ws')
-    init_emitter(client)
+    client = None
+    try:
+        socketIO = SocketIO('127.0.0.1', 80, Namespace=BaseNamespace, wait_for_connection=False)
+        client = socketIO.define(BaseNamespace, '/train-ws')
+        init_emitter(client)
+
+    except Exception:
+        print(f'Cannot connect to socket.io server, running in console mode')
 
     try:
         train_model(use_gpu == 'True' or use_gpu is True)
+
+    except KeyboardInterrupt:
+        print(f'Stopped by keyboard interrupt')
 
     except Exception:
         print(traceback.format_exc())
 
     finally:
-        client.emit('stop')
+        if client is not None:
+            client.emit('stop')
 
 
 if __name__ == '__main__':
