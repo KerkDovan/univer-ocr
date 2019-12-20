a29b46df864166459c449fb13f8def7647e401d6
Kerk Dovan <bogomolov.yuriy@gmail.com>
2019-12-20 19:41:03 +0300

    Apply @staticmethod decorator to methods of CP.

--- web_app/components/nn/gpu.py
+++ web_app/components/nn/gpu.py
@@ -6,19 +6,23 @@ class CP:
     cp = numpy
     is_gpu_used = False
 
+    @staticmethod
     def use_cpu():
         CP.cp = numpy
         CP.is_gpu_used = False
 
+    @staticmethod
     def use_gpu():
         CP.cp = cupy
         CP.is_gpu_used = True
 
+    @staticmethod
     def copy(obj):
         if CP.is_gpu_used:
             return cupy.asarray(obj)
         return numpy.copy(obj)
 
+    @staticmethod
     def asnumpy(obj):
         if CP.is_gpu_used:
             return cupy.asnumpy(obj)
